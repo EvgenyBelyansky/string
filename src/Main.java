5a884ef594f50ad1fa70febc7194d07e63d6b161
EvgenyBelyansky <e.g.belyansky@gmail.com>
2024-05-04 10:23:49 +0400

    Начальный комит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        String phone = "+960 415 75 37";
+        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телефон слишком короткий");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Среди нас посторонний");
+        }
+        System.out.println("phone = " + phone);
 
+        String expectedPhone = "79604157537";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
+
+//        String address = "Адрес который мы хотим указать при доставке";
+//        String street = "Манежная";
+//        street = "Манежная площадь";
+//        System.out.println("Адрес магазина - " + street);
     }
 }
